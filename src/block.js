ba39e917b855420a845777c1a22b4aa6e4f2decc
Niels Lange <info@nielslange.de>
2021-10-21 11:37:49 +0700

    Show dynamic cart totals in frontend

--- src/block.js
+++ src/block.js
@@ -3,7 +3,22 @@
  */
 import { __, sprintf } from '@wordpress/i18n';
 
-export default function Block( { currentTotal, freeShippingFrom } ) {
+export default function Block( { freeShippingFrom, cart } ) {
+	let currentTotal;
+	let currencyPrefix;
+	let currencySuffix;
+
+	if ( undefined === cart ) {
+		currentTotal = 45;
+		currencyPrefix = '$';
+		currencySuffix = '';
+	} else {
+		currentTotal = cart.cartTotals.total_price / 100;
+		currencyPrefix = cart.cartTotals.currency_prefix;
+		currencySuffix = cart.cartTotals.currency_suffix;
+		console.log( cart );
+	}
+
 	const progress = ( currentTotal / freeShippingFrom ) * 100;
 	const divWidth = ( progress > 100 ? 100 : progress ) + '%';
 	const divStyle = { width: divWidth };
@@ -12,10 +27,12 @@ export default function Block( { currentTotal, freeShippingFrom } ) {
 		remaining > 0
 			? sprintf(
 					__(
-						'Spend only $%s more to get free US shipping!',
+						'Spend only %s%s%s more to get free shipping!',
 						'free-shipping-progress-bar'
 					),
-					remaining
+					currencyPrefix,
+					remaining,
+					currencySuffix
 			  )
 			: __(
 					'You have qualified for free shipping. Great job!',
