853edec07d9347155449e67b798b590db57a78f1
Niels Lange <info@nielslange.de>
2021-10-21 18:42:26 +0700

    Remove currencyPrefix and currencySuffix from frontend

--- src/block.js
+++ src/block.js
@@ -5,18 +5,11 @@ import { __, sprintf } from '@wordpress/i18n';
 
 export default function Block( { freeShippingFrom, cart } ) {
 	let currentTotal;
-	let currencyPrefix;
-	let currencySuffix;
 
 	if ( undefined === cart ) {
 		currentTotal = 45;
-		currencyPrefix = '$';
-		currencySuffix = '';
 	} else {
 		currentTotal = cart.cartTotals.total_price / 100;
-		currencyPrefix = cart.cartTotals.currency_prefix;
-		currencySuffix = cart.cartTotals.currency_suffix;
-		console.log( cart );
 	}
 
 	const progress = ( currentTotal / freeShippingFrom ) * 100;
@@ -27,12 +20,10 @@ export default function Block( { freeShippingFrom, cart } ) {
 		remaining > 0
 			? sprintf(
 					__(
-						'Spend only %s%s%s more to get free shipping!',
+						'Spend only $%s more to get free shipping!',
 						'free-shipping-progress-bar'
 					),
-					currencyPrefix,
-					remaining,
-					currencySuffix
+					remaining
 			  )
 			: __(
 					'You have qualified for free shipping. Great job!',
