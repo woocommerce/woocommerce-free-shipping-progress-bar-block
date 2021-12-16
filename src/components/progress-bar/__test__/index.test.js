65985ae78fea051a5411b7ac7408daf0f69984b9
Niels Lange <info@nielslange.de>
2021-12-16 15:27:45 +0700

    Remove obsolete test case and replace getByTestId() with querySelector()

--- src/components/progress-bar/__test__/index.test.js
+++ src/components/progress-bar/__test__/index.test.js
@@ -11,59 +11,37 @@ import '@testing-library/jest-dom';
  */
 import ProgressBar from './../index';
 
-describe( 'The ProgressBar component', () => {
-	it( 'renders without crashing', () => {
-		const div = document.createElement( 'div' );
-		ReactDOM.render( <ProgressBar></ProgressBar>, div );
-	} );
+const outer = '.wc-free-shipping-progress-bar__outer';
+const inner = '.wc-free-shipping-progress-bar__inner';
 
+describe( 'The ProgressBar component', () => {
 	it( 'shows the classnames of the outer div correctly', () => {
-		const { getByTestId } = render( <ProgressBar></ProgressBar> );
-		expect( getByTestId( 'progress-bar-outer' ) ).toHaveClass(
-			'wc-free-shipping-progress-bar__outer'
-		);
+		render( <ProgressBar /> );
+		expect( document.querySelector( outer ) ).toBeInTheDocument();
 	} );
 
 	it( 'shows the classnames of the inner div correctly', () => {
-		const { getByTestId } = render( <ProgressBar></ProgressBar> );
-		expect( getByTestId( 'progress-bar-inner' ) ).toHaveClass(
-			'wc-free-shipping-progress-bar__inner'
-		);
+		render( <ProgressBar /> );
+		expect( document.querySelector( inner ) ).toBeInTheDocument();
 	} );
 
 	it( 'shows the style for a full bar correctly', () => {
-		const { getByTestId } = render(
-			<ProgressBar freeShippingFrom="4" currentTotal="4"></ProgressBar>
-		);
-		expect( getByTestId( 'progress-bar-inner' ) ).toHaveStyle(
-			'width: 100%'
-		);
+		render( <ProgressBar freeShippingFrom="4" currentTotal="4" /> );
+		expect( document.querySelector( inner ) ).toHaveStyle( 'width: 100%' );
 	} );
 
 	it( 'shows the style for a threequarter bar correctly', () => {
-		const { getByTestId } = render(
-			<ProgressBar freeShippingFrom="4" currentTotal="3"></ProgressBar>
-		);
-		expect( getByTestId( 'progress-bar-inner' ) ).toHaveStyle(
-			'width: 75%'
-		);
+		render( <ProgressBar freeShippingFrom="4" currentTotal="3" /> );
+		expect( document.querySelector( inner ) ).toHaveStyle( 'width: 75%' );
 	} );
 
 	it( 'shows the style for a half bar correctly', () => {
-		const { getByTestId } = render(
-			<ProgressBar freeShippingFrom="4" currentTotal="2"></ProgressBar>
-		);
-		expect( getByTestId( 'progress-bar-inner' ) ).toHaveStyle(
-			'width: 50%'
-		);
+		render( <ProgressBar freeShippingFrom="4" currentTotal="2" /> );
+		expect( document.querySelector( inner ) ).toHaveStyle( 'width: 50%' );
 	} );
 
 	it( 'shows the style for an empty bar correctly', () => {
-		const { getByTestId } = render(
-			<ProgressBar freeShippingFrom="4" currentTotal="0"></ProgressBar>
-		);
-		expect( getByTestId( 'progress-bar-inner' ) ).toHaveStyle(
-			'width: 0%'
-		);
+		render( <ProgressBar freeShippingFrom="4" currentTotal="0" /> );
+		expect( document.querySelector( inner ) ).toHaveStyle( 'width: 0%' );
 	} );
 } );
--- src/components/progress-label/__test__/index.test.js
+++ src/components/progress-label/__test__/index.test.js
@@ -11,41 +11,36 @@ import '@testing-library/jest-dom';
  */
 import ProgressLabel from '../index';
 
-describe( 'The ProgressLabel component', () => {
-	it( 'renders without crashing', () => {
-		const div = document.createElement( 'div' );
-		ReactDOM.render( <ProgressLabel></ProgressLabel>, div );
-	} );
+const label = '.wc-free-shipping-progress-bar__label';
 
+describe( 'The ProgressLabel component', () => {
 	it( 'shows the classnames of the div correctly', () => {
-		const { getByTestId } = render( <ProgressLabel></ProgressLabel> );
-		expect( getByTestId( 'progress-bar-label' ) ).toHaveClass(
-			'wc-free-shipping-progress-bar__label'
-		);
+		render( <ProgressLabel /> );
+		expect( document.querySelector( label ) ).toBeInTheDocument();
 	} );
 
 	it( 'shows the message for insufficient totals correctly', () => {
-		const { getByTestId } = render(
+		render(
 			<ProgressLabel
 				labelInsufficientTotals="Spend only {amount} more to get free shipping!"
 				freeShippingFrom="2"
 				currentTotal="1"
-			></ProgressLabel>
+			/>
 		);
-		expect( getByTestId( 'progress-bar-label' ) ).toHaveTextContent(
+		expect( document.querySelector( label ) ).toHaveTextContent(
 			'Spend only 1.00 more to get free shipping!'
 		);
 	} );
 
 	it( 'shows the message for sufficient totals correctly', () => {
-		const { getByTestId } = render(
+		render(
 			<ProgressLabel
 				labelSufficientTotals="You have qualified for free shipping. Great job!"
 				freeShippingFrom="2"
 				currentTotal="2"
-			></ProgressLabel>
+			/>
 		);
-		expect( getByTestId( 'progress-bar-label' ) ).toHaveTextContent(
+		expect( document.querySelector( label ) ).toHaveTextContent(
 			'You have qualified for free shipping. Great job!'
 		);
 	} );
