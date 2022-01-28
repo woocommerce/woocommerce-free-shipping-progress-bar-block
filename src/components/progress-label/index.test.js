995f5a0273417915909396a652f25faa74a1e34d
Niels Lange <info@nielslange.de>
2022-01-28 21:03:33 +0700

    Fix failing integration test

--- src/components/progress-label/index.test.js
+++ src/components/progress-label/index.test.js
@@ -12,7 +12,7 @@ import ProgressLabel from './index';
 jest.mock(
 	'@woocommerce/price-format',
 	() => ( {
-		formatPrice: jest.fn().mockImplementation( ( value ) => '$100.00' ),
+		formatPrice: jest.fn().mockImplementation( () => '$100.00' ),
 		getCurrencyFromPriceResponse: jest.fn().mockImplementation( () => ( {
 			code: 'USD',
 			prefix: '$',
@@ -24,35 +24,48 @@ jest.mock(
 const label = '.wc-free-shipping-progress-bar__label';
 
 describe( 'The ProgressLabel component', () => {
+	/**
+	 * Test is the <ProgressLabel /> component renderes correctly without props.
+	 */
 	it( 'shows the classnames of the div correctly', () => {
 		render( <ProgressLabel /> );
 		expect( document.querySelector( label ) ).toBeInTheDocument();
 	} );
 
+	/**
+	 * Test is the <ProgressLabel /> component shows the correct message for the
+	 * insufficient totals when shipping is free from $200.00, but the total
+	 * cart value is $100.00.
+	 */
 	it( 'shows the message for insufficient totals correctly', () => {
-		const cart = { cartTotals: { total_price: 10000 } };
 		const labelBefore = 'Spend only {amount} more to get free shipping!';
 		const labelAfter = 'Spend only $100.00 more to get free shipping!';
 		const args = {
-			labelSufficientTotals: labelBefore,
 			freeShippingFrom: 200,
-			cart: cart,
+			labelInsufficientTotals: labelBefore,
+			cart: {
+				cartTotals: { total_price: 100, currency_minor_unit: 0 },
+			},
 		};
 
 		render( <ProgressLabel { ...args } /> );
 		expect( screen.getByText( labelAfter ) ).toBeInTheDocument();
 	} );
 
+	/**
+	 * Test is the <ProgressLabel /> component shows the correct message for the
+	 * sufficient totals when shipping is free from $100.00, and the total cart
+	 * value equals $100.00.
+	 */
 	it( 'shows the message for sufficient totals correctly', () => {
-		const cart = { cartTotals: { total_price: 10000 } };
 		const label = 'You have qualified for free shipping. Great job!';
 		const args = {
-			labelSufficientTotals: label,
 			freeShippingFrom: 100,
-			cart: cart,
+			labelSufficientTotals: label,
+			cart: { cartTotals: { total_price: 100, currency_minor_unit: 0 } },
 		};
 
-		render( <ProgressLabel { ...args } /> );
+		const progressbar = render( <ProgressLabel { ...args } /> );
 		expect( screen.getByText( label ) ).toBeInTheDocument();
 	} );
 } );
